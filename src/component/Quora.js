5be2bd944c321645f23d073fa43ff2f3a20e4bbc
Anurag Patel <anuragpatel1281@gmail.com>
2021-05-28 17:05:22 +0530

    3rd commit

---
+++ src/component/Quora.js
@@ -0,0 +1,13 @@
+import React from "react";
+import '../css/Quora.css';
+import Navbar from "./Navbar";
+function Quora() {
+  return (
+    <div className ="quora">
+      Hello from the quora container component.
+      <Navbar/>
+    </div>
+  );
+}
+
+export default Quora;
